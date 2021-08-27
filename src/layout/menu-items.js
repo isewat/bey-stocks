36dcb68092836bf31cb77a2709a8947b54343417
Ralf Siewert <isewat@gmail.com>
2021-08-27 21:53:41 +0200

    remove demo pages

--- src/layout/menu-items.js
+++ src/layout/menu-items.js
@@ -3,22 +3,22 @@ import WidgetsIcon from '@material-ui/icons/Widgets';
 import LockOpenIcon from '@material-ui/icons/LockOpen';
 
 export const menuItems = [
+  // {
+  //   text: 'Typography Demo',
+  //   icon: <TextFieldsIcon color="primary" />,
+  //   location: {
+  //     pathname: '/',
+  //   }
+  // },
+  // {
+  //   text: 'Component Demo',
+  //   icon: <WidgetsIcon color="primary" />,
+  //   location: {
+  //     pathname: '/component-demo',
+  //   }
+  // },
   {
-    text: 'Typography Demo',
-    icon: <TextFieldsIcon color="primary" />,
-    location: {
-      pathname: '/',
-    }
-  },
-  {
-    text: 'Component Demo',
-    icon: <WidgetsIcon color="primary" />,
-    location: {
-      pathname: '/component-demo',
-    }
-  },
-  {
-    text: 'PrivatePage',
+    text: 'Profil',
     icon: <LockOpenIcon color="primary" />,
     private: true,
     location: {
